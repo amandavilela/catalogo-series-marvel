2e3b83b96e306fb7a1b50e5cd0a046dbe646484c
amanda <amandavilelaalmeida@gmail.com>
2018-02-19 18:45:22 -0300

    Add initial structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Marvel Series Catalog
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/MarvelLogo.svg" className="App-logo" alt="logo" />
+          <h1 className="App-title">Marvel Series</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
